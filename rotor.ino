26d374c8ac5835763b8889f2a975c3ad745af359
user736 <user736@rambler.ru>
2018-08-15 00:31:07 +0300

    Update rotor.ino

--- rotor.ino
+++ rotor.ino
@@ -1,4 +1,5 @@
-#include <LiquidCrystal.h>
+//#include <LiquidCrystal.h>
+#include <UTFT.h>
 #include <max6675.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
@@ -10,21 +11,27 @@
 #define LCD_D6 33
 #define LCD_D7 31
 
-#define thermoDO 46
-#define thermoCS 48
-#define thermoCLK 50
-#define clockHC 44
-#define latchHC 42
-#define dataHC 40
+#define thermoDO 5 
+//46
+#define thermoCS 6 
+//48
+#define thermoCLK 7 
+//50
+#define clockHC 4 
+//44
+#define latchHC 3 
+//42
+#define dataHC 2 
+//40
 #define i_temp_head 0
 #define i_temp_cool_in 1
 #define i_temp_cool_out 2
 #define temps_count 4
 
 #define OCR2A_v 110
-#define pin_B 49
+#define pin_B 16
 #define temp2start 20
-#define pin_pwm 29
+#define pin_pwm 17
 #define averaging 8
 #define tiks_per_m 29296
 #define rele_pwm_val 1536
@@ -36,7 +43,7 @@
 #define ps_in 14
 #define ps_out 15
 
-LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+//LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
 
@@ -46,7 +53,7 @@ int shift_data;
 
 int RPM=0;
 int rpms[averaging+1];
-int amperages[averaging+1];
+int amperages[averaging*5+1];
 int i_int=0;
 int tiks_int2d=0;
 int tiks_int=0;
@@ -64,19 +71,32 @@ int load_pwm_val=512;
 int fan_pwm_val=512;
 int accel_tiks=0;
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+char buffer[5];
+
+extern uint8_t BigFont[];
+UTFT myGLCD(CTE32HR,38,39,40,41);
 
 void setup() {
     Serial.begin(9600);
+
+    // Setup the LCD
+  myGLCD.InitLCD();
+  myGLCD.setFont(BigFont);
+  myGLCD.clrScr();
+  myGLCD.setColor(0, 255, 0);
+  //myGLCD.fillRect(0, 0, 479, 319);
+  myGLCD.print("* ROTOR SUMY *", CENTER, 1);
+
   
-    lcd.begin(20, 4);
-    lcd.setCursor(2, 0);
-    lcd.write("** ROTOR SUMY **");
-    lcd.setCursor(0, 1);
-    lcd.write("P1-    P2-    I-");
-    lcd.setCursor(0, 2);
-    lcd.write("T1-    T2-    T3-");
-    lcd.setCursor(0, 3);
-    lcd.write("RPM-      STATE-");
+    //lcd.begin(20, 4);
+    //lcd.setCursor(2, 0);
+    //lcd.write("** ROTOR SUMY **");
+    //lcd.setCursor(0, 1);
+    myGLCD.print("U1-      U2-      I-",1 ,20);
+    //lcd.setCursor(0, 2);
+    myGLCD.print("T1-      T2-      T3-", 1, 37);
+    //lcd.setCursor(0, 3);
+    myGLCD.print("RPM-      STATE-" , 1 , 54);
 
     for(int i = 0; i < temps_count; i++){
         temps[i]=0;
@@ -87,6 +107,9 @@ void setup() {
 
     for (int i=1; i<averaging+1; i++){
         rpms[i]=0;
+    }
+    for (int i=1; i<averaging*5+1; i++){
+        amperages[i]=0;
     }
     pinMode(pin_B, INPUT_PULLUP);
     pinMode(pin_pwm,OUTPUT);
@@ -126,11 +149,13 @@ void setup() {
     EIFR=(0<<INTF7) | (0<<INTF6) | (0<<INTF5) | (0<<INTF4) | (1<<INTF3) | (0<<INTF2) | (0<<INTF1) | (0<<INTF0);
 
     pwm.begin();
-    pwm.setPWMFreq(1000);
-    Wire.setClock(200000);
+    pwm.setPWMFreq(1600);
+    Wire.setClock(100000);
     for (int i=0; i<16; i++){
       pwm.setPWM(i,0,4096); 
     }
+
+    analogReference(EXTERNAL);
 }
 
 ISR (TIMER2_COMPA_vect) {
@@ -167,16 +192,18 @@ void set_amperage(int val){
 
 void set_averaging(int index, int val){  
     int *temp_arr;
+    int aver=averaging;
     if (index==0){
       val=tiks_per_m/val;
       temp_arr=rpms;
     }else{
       temp_arr=amperages;
+      aver=aver*5;
     }
     
     int s=0;
     int c=0;
-    for (int i=1; i<averaging; i++){
+    for (int i=1; i<aver; i++){
         temp_arr[i-1]=temp_arr[i];
         if(temp_arr[i]==0){
             s=0;
@@ -188,7 +215,7 @@ void set_averaging(int index, int val){
     }
     s+=val;
     c++;
-    temp_arr[averaging]=s/c;
+    temp_arr[aver]=s/c;
 }
 
 void loop() {
@@ -245,6 +272,7 @@ void loop() {
         }
         pwm.setPWM(pwm_fan, 0, fan_pwm_val);
         pwm.setPWM(pwm_load, 0, load_pwm_val);
+        Serial.println("test");
     }else{
         if (accelerated){
             accel_tiks++;
@@ -296,39 +324,43 @@ void loop() {
             tiks2start_error=0;
         }
     }
-    lcd.setCursor(16, 1);
-    lcd.print("    ");
-    lcd.setCursor(16, 1);
-    lcd.print((float)(511-amperages[averaging])*100/1024);
-    lcd.setCursor(3, 2);
-    lcd.print("    ");
-    lcd.setCursor(3, 2);
-    lcd.print(temps[i_temp_head]);
-    lcd.setCursor(10, 2);
-    lcd.print("    ");
-    lcd.setCursor(10, 2);
-    lcd.print(temps[i_temp_cool_in]);
-    lcd.setCursor(17, 2);
-    lcd.print("    ");
-    lcd.setCursor(17, 2);
-    lcd.print(temps[i_temp_cool_out]);
-    lcd.setCursor(4,3);
-    lcd.print("      ");
-    lcd.setCursor(4,3);
-    lcd.print(rpms[averaging]);
-    lcd.setCursor(16,3);
-    lcd.print("    ");
-    lcd.setCursor(16,3);
-    lcd.print(ready2start);
-    lcd.print(started);
-    lcd.print(accelerated);
-    lcd.print(start_error);
+    //lcd.setCursor(16, 1);
+    //lcd.print("    ");
+    //lcd.setCursor(16, 1);
+    //dtostrf((float)(511-amperages[averaging*5])*100/512, buffer, 5,1);
+    //myGLCD.print(buffer, 16*22, 20);
+    //lcd.setCursor(3, 2);
+    //lcd.print("    ");
+    //lcd.setCursor(3, 2);
+    myGLCD.printNumI(temps[i_temp_head], 16*4, 37);
+    //lcd.setCursor(10, 2);
+    //lcd.print("    ");
+    //lcd.setCursor(10, 2);
+    myGLCD.printNumI(temps[i_temp_cool_in], 16*13, 37);
+    //lcd.setCursor(17, 2);
+    //lcd.print("    ");
+    //lcd.setCursor(17, 2);
+    myGLCD.printNumI(temps[i_temp_cool_out], 16*22, 37);
+    //lcd.setCursor(4,3);
+    //lcd.print("      ");
+    //lcd.setCursor(4,3);
+    myGLCD.printNumI(rpms[averaging], 16*5, 54);
+    //lcd.setCursor(16,3);
+    //lcd.print("    ");
+    //lcd.setCursor(16,3);
+    myGLCD.printNumI(ready2start, 16*17, 54);
+    myGLCD.printNumI(started, 16*18, 54);
+    myGLCD.printNumI(accelerated, 16*19, 54);
+    myGLCD.printNumI(start_error, 16*20, 54);
     
     temps[i_temp]=thermocouple.readCelsius();
     Serial.print(i_temp);
     Serial.print("-");
     Serial.println(temps[i_temp]);
+    Serial.println((float)(511-amperages[averaging*5])*100/512);
+    Serial.println(analogRead(A5));
+    Serial.println(analogRead(A6));
+    Serial.println(analogRead(A7));
     Serial.println(analogRead(A0));
-
     
 }
